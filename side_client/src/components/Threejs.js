26440231ac70d0a8699a9047cc6064ff38533b16
nonassembly <nonassembly@naver.com>
2021-01-31 17:41:01 +0900

    commit

--- side_client/src/components/Threejs.js
+++ side_client/src/components/Threejs.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
 
 class Threejs extends Component{
     componentDidMount(){
         const scene=new THREE.Scene();
         const camera=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        camera.position.z=5;
+        camera.position.x=0;
+        camera.position.y=0;
+        camera.position.z=100;
         
-        const loader=new GLTFLoader();
-        loader.load('http://localhost:8000/universe.gltf', function(gltf){
-            scene.add(gltf.scene);
+        const loader=new OBJLoader();
+        loader.load('http://localhost:8000/universe.obj', function(object){
+            scene.add(object);
         }, undefined, function(error){
             console.error(error)
         })
@@ -21,6 +23,8 @@ class Threejs extends Component{
 
         function animate() {
             requestAnimationFrame( animate );
+            //scene.rotation.y+=0.01
+            scene.rotateOnAxis(new THREE.Vector3(0, 0.5, 0), 0.005)
             renderer.render( scene, camera );
         }
         animate();
--- side_server/server.js
+++ side_server/server.js
@@ -2,21 +2,8 @@ const express=require('express')
 const fs=require('fs')
 const app=express()
 
-app.get('/universe.gltf', function(req, res){
-    fs.readFile('./universe.gltf', function(err, data){
-        res.writeHead(200, {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-            "Access-Control-Max-Age":"3600",
-            "Access-Control-Allow-Headers":"Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-        });
-        res.write(data);
-        res.end();
-    })
-})
-
-app.get('/scene.bin', function(req, res){
-    fs.readFile('./scene.bin', function(err, data){
+app.get('/universe.obj', function(req, res){
+    fs.readFile('./universe.obj', function(err, data){
         res.writeHead(200, {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
