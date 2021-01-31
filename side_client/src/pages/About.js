98b0e1beaab4a97a917cd7652a44caaae9c8bd34
nonassembly <nonassembly@naver.com>
2021-02-01 06:00:49 +0900

    commit

---
+++ side_client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Header from '../components/Header';
+
+class About extends Component{
+    render(){
+        return(
+            <>
+            <Header />
+            </>
+        )
+    }
+}
+
+export default About;
---
+++ side_client/src/pages/Chapters.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Header from '../components/Header';
+
+class Chapters extends Component{
+    render(){
+        return(
+            <>
+            <Header />
+            </>
+        )
+    }
+}
+
+export default Chapters;
